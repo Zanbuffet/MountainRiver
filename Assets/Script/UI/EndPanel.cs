e785378dd540c666cbf19f97857b03fee8feed44
Damien He <40878295+Mesdwe@users.noreply.github.com>
2022-10-20 13:15:04 +0100

    Level Canvas update

---
+++ Assets/Script/UI/EndPanel.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class EndPanel : MonoBehaviour
+{
+    private LevelManager levelManager;
+    [SerializeField] private GameObject endPanel;
+    [SerializeField] private Toggle goal1;
+    [SerializeField] private Toggle goal2;
+    [SerializeField] private Toggle goal3;
+
+    [SerializeField] private Image star1;
+    [SerializeField] private Image star2;
+    [SerializeField] private Image star3;
+    
+    [SerializeField] private Sprite[] starSprites;
+    
+    public void OnWin()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+        endPanel.SetActive(true);
+        star1.sprite = levelManager.lvObject.FirstStar ? starSprites[1] : starSprites[0];
+        goal1.isOn = levelManager.lvObject.FirstStar ? true : false;
+        star2.sprite = levelManager.lvObject.SecondStar ? starSprites[1] : starSprites[0];
+        goal2.isOn = levelManager.lvObject.SecondStar ? true : false;
+        star3.sprite = levelManager.lvObject.ThirdStar ? starSprites[1] : starSprites[0];
+        goal3.isOn = levelManager.lvObject.ThirdStar ? true : false;
+    }
+
+    public void NextLevel()
+    {
+        endPanel.SetActive(false);
+        int index = SceneManager.GetActiveScene().buildIndex + 1;
+
+        if (index >= SceneManager.sceneCountInBuildSettings) SceneManager.LoadScene(0);
+        else
+            SceneManager.LoadScene(index);
+    }
+    private void Start()
+    {
+        //OnWin();//test
+    }
+}
